fddd2bcaa4a735b01edfdedddcf502e93ece9f47
DimancheRouge <71337710+DimancheRouge@users.noreply.github.com>
2021-01-14 23:06:14 -0500

    Bug fixing

--- CheckersGame.cpp
+++ CheckersGame.cpp
@@ -16,112 +16,112 @@ void CheckersGame::translate(char startArray[2], char endArray[2], int startConv
 
 	switch (startArray[0]) {
 		case 'A':
-			startConvert[0] = 1;
+			startConvert[0] = 0;
 			break;
 		case 'B':
-			startConvert[0] = 2;
+			startConvert[0] = 1;
 			break;
 		case 'C':
-			startConvert[0] = 3;
+			startConvert[0] = 2;
 			break;
 		case 'D':
-			startConvert[0] = 4;
+			startConvert[0] = 3;
 			break;
 		case 'E':
-			startConvert[0] = 5;
+			startConvert[0] = 4;
 		case 'F':
-			startConvert[0] = 6;
+			startConvert[0] = 5;
 			break;
 		case 'G':
-			startConvert[0] = 7;
+			startConvert[0] = 6;
 			break;
 		case 'H':
-			startConvert[0] = 8;
+			startConvert[0] = 7;
 			break;
 		default:
-			startConvert[0] = 0;
+			startConvert[0] = 8;
 	}
 	switch (startArray[1]) {
 		case '1':
-			startConvert[1] = 1;
+			startConvert[1] = 0;
 			break;
 		case '2':
-			startConvert[1] = 2;
+			startConvert[1] = 1;
 			break;
 		case '3':
-			startConvert[1] = 3;
+			startConvert[1] = 2;
 			break;
 		case '4':
-			startConvert[1] = 4;
+			startConvert[1] = 3;
 			break;
 		case '5':
-			startConvert[1] = 5;
+			startConvert[1] = 4;
 		case '6':
-			startConvert[1] = 6;
+			startConvert[1] = 5;
 			break;
 		case '7':
-			startConvert[1] = 7;
+			startConvert[1] = 6;
 			break;
 		case '8':
-			startConvert[1] = 8;
+			startConvert[1] = 7;
 			break;
 		default:
-			startConvert[1] = 0;
+			startConvert[1] = 8;
 	}
 	
 	switch (endArray[0]) {
 		case 'A':
-			endConvert[0] = 1;
+			endConvert[0] = 0;
 			break;
 		case 'B':
-			endConvert[0] = 2;
+			endConvert[0] = 1;
 			break;
 		case 'C':
-			endConvert[0] = 3;
+			endConvert[0] = 2;
 			break;
 		case 'D':
-			endConvert[0] = 4;
+			endConvert[0] = 3;
 			break;
 		case 'E':
-			endConvert[0] = 5;
+			endConvert[0] = 4;
 		case 'F':
-			endConvert[0] = 6;
+			endConvert[0] = 5;
 			break;
 		case 'G':
-			endConvert[0] = 7;
+			endConvert[0] = 6;
 			break;
 		case 'H':
-			endConvert[0] = 8;
+			endConvert[0] = 7;
 			break;
 		default:
-			endConvert[0] = 0;
+			endConvert[0] = 8;
 	}
 	switch (endArray[1]) {
 		case '1':
-			endConvert[1] = 1;
+			endConvert[1] = 0;
 			break;
 		case '2':
-			endConvert[1] = 2;
+			endConvert[1] = 1;
 			break;
 		case '3':
-			endConvert[1] = 3;
+			endConvert[1] = 2;
 			break;
 		case '4':
-			endConvert[1] = 4;
+			endConvert[1] = 3;
 			break;
 		case '5':
-			endConvert[1] = 5;
+			endConvert[1] = 4;
 		case '6':
-			endConvert[1] = 6;
+			endConvert[1] = 5;
 			break;
 		case '7':
-			endConvert[1] = 7;
+			endConvert[1] = 6;
 			break;
 		case '8':
-			endConvert[1] = 8;
+			endConvert[1] = 7;
 			break;
 		default:
-			endConvert[1] = 0;
+			endConvert[1] = 8;
 	}
 }
 
